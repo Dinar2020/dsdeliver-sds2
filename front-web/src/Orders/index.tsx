8b01c83c7a4b372e50aba5d79333da9e1f58e219
Dinario Rebouças <dinario@outlook.com.br>
2021-01-08 19:18:48 -0300

    Add routes and StepsHeader

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+import {ReactComponent as Logo} from './logo.svg';
+import StepsHearder from './StepsHeader';
+
+function Orders() {
+    return (
+        <div className="orders-container">
+            <StepsHearder/>
+        </div>
+    )
+}
+
+export default Orders;
